766dd3560d5857b44db6f65143ff80bcd190b55f
Nicholas Fiore <nicholas.fiore1@marist.edu>
2024-02-05 10:44:21 -0500

    removed the whitespace remover
    turns out that's not helpful nor necessary

--- dist/scripts/lexer.js
+++ dist/scripts/lexer.js
@@ -8,10 +8,7 @@ var TSC;
                 let sourceCode = document.getElementById("taSourceCode").value;
                 // Trim the leading and trailing spaces.
                 sourceCode = TSC.Utils.trim(sourceCode);
-                console.log(sourceCode);
-                // Removes all whitespace characters. Sourced from: https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
-                sourceCode = sourceCode.replace(/\s/g, '');
-                console.log(sourceCode);
+                
                 return sourceCode;
             }
         }
