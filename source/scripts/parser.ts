1b8566fc2b0157c2f68edbf24a8efbc1ce20be83
Nicholas Fiore <nicholas.fiore1@marist.edu>
2024-02-16 12:44:36 -0500

    set up parser skeleton
    using the term "skeleton has gotten a tally hall song stuck in my head

---
+++ source/scripts/parser.ts
@@ -0,0 +1,103 @@
+/* Parser.ts */
+
+class Parser extends Component {
+    private tokens: Array<Token>;
+
+    constructor(tokenStream: Array<Token>) {
+        super('Parser');
+
+        this.tokens = tokenStream;
+    }
+
+    private match(desiredToken: string) {
+
+    }
+
+    private parseStart() {
+        
+    }
+
+    private parseBlock() {
+
+    }
+
+    private parseStatementList() {
+
+    }
+
+    private parseStatement() {
+
+    }
+
+    private parsePrintStatement() {
+
+    }
+
+    private parseAssignmentStatement() {
+
+    }
+
+    private parseVarDeclaration() {
+
+    }
+
+    private parseWhileStatement() {
+
+    }
+
+    private parseIfStatement() {
+
+    }
+
+    private parseExpression() {
+
+    }
+
+    private parseIntExpression() {
+
+    }
+
+    private parseStringExpression() {
+
+    }
+
+    private parseBooleanExpression() {
+
+    }
+
+    private parseID() {
+
+    }
+
+    private parseCharList() {
+
+    }
+
+    private parseType() {
+
+    }
+
+    private parseChar() {
+
+    }
+
+    private parseSpace() {
+
+    }
+
+    private parseDigit() {
+
+    }
+
+    private parseBooleanOperation() {
+
+    }
+
+    private parseBooleanValue() {
+
+    }
+
+    private parseIntegerOperation() {
+        
+    }
+}
