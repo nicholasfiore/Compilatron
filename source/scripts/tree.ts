0ae3d53ec69fc4d2f5a03fd21254e494c55ced3d
Nicholas Fiore <nicholas.fiore1@marist.edu>
2024-02-19 11:33:55 -0500

    setters for node

--- source/scripts/tree.ts
+++ source/scripts/tree.ts
@@ -10,6 +10,10 @@ class Tree {
     private addNode(label: string) {
         var node = new TreeNode(label);
         if (this.root === null) {
+            this.root = node;
+            node.setParent(null);
+        }
+        else {
             
         }
     }
@@ -23,5 +27,13 @@ class TreeNode {
 
     constructor(newName: string) {
         this.name = newName;
+    }
+
+    public setParent(node: TreeNode) {
+        this.parent = node;
+    }
+
+    public addChild(node: TreeNode) {
+        this.children.push(node);
     }
 }
