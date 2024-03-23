25e62e1c89b1426bfe34818d5273bfc8e2cba31c
nicholas.fiore1@marist.edu <nicholas.fiore1@marist.edu>
2024-03-23 14:36:37 -0400

    switching gears; going to try to build the AST from the token stream (even though I don't like it, it likely will be easier)

--- source/scripts/analyzer.ts
+++ source/scripts/analyzer.ts
@@ -2,19 +2,25 @@
 class SemanticAnalyzer extends Component {
     
     private CST: Tree;
+    private tokens: Array<Token>;
     private AST: Tree;
 
-    constructor(ConcreteSyntaxTree: Tree, enableDebug: boolean) {
+    constructor(ConcreteSyntaxTree: Tree, TokenStream: Array<Token>, enableDebug: boolean) {
         super("Semantic Analyzer", enableDebug);
         this.AST = new Tree("AST");
         this.CST = ConcreteSyntaxTree;
+        this.tokens = TokenStream;
     }
 
     public analyze() {
-        this.AST.buildAST(this.CST.getRoot());
+        //this.AST.buildAST(this.CST.getRoot());
+        this.buildAST();
         this.info("Abstract Syntax Tree:");
         this.AST.printTree(this.AST.getRoot());
     }
 
+    private buildAST() {
+
+    }
     
 }
