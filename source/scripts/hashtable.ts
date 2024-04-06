6d5fdc4f94d6fdb1d24b8a48d1b54baa1ccce1e5
nicholas.fiore1@marist.edu <nicholas.fiore1@marist.edu>
2024-04-05 20:52:15 -0400

    more setup for the hash table

--- source/scripts/hashtable.ts
+++ source/scripts/hashtable.ts
@@ -1,4 +1,38 @@
 class HashTable extends Component {
+    private static readonly HASH_TABLE_SIZE = 26;
+
+    private table: Array<HashEntry>;
+
+    constructor(name: string) {
+        super(name, false);
+    }
+
+    public makeHashCode(input: string) {
+        var hash = input.charCodeAt(0) - 65 + "";
+        return hash;
+    }
+
+    public put(key: string) {
+        var hash = this.makeHashCode(key);
+        var entry = new HashEntry(key);
+        if (!this.table[hash]) {
+            this.table[hash] = entry;
+        } else {
+            this.err("Redeclaration error");
+        }
+    }
+}
+
+class HashEntry {
+    private hash;
+    private id;
+    private type;
+    private isInit;
+    private hasBeenUsed;
+
+    constructor(id: string) {
+        this.id = id;
+    }
 
 }
 
@@ -8,7 +42,7 @@ class HashTree extends Component {
 
     constructor(name: string) {
         super(name, false);
-        
+
     }
 
     public addNode(table: HashTable) {
