154d07248eb7802220a0b824e9f03e0dbf58741d
Nicholas Fiore <nicholas.fiore1@marist.edu>
2024-04-09 12:43:35 -0400

    initial setup for the generator

--- source/scripts/generator.ts
+++ source/scripts/generator.ts
@@ -1,8 +1,17 @@
 class Generator extends Component {
+    private AST: Tree;
+    private symbolTable;
     
+    private static readonly MAX_BYTES_MEMORY: number = 256;
+
+    private machineCode: string[];
 
 
     constructor(AST: Tree, symbolTable: HashTree, debug: boolean) {
         super("Code Generator", debug);
+        this.AST = AST;
+        this.symbolTable = symbolTable;
+        
+        this.machineCode = Array(Generator.MAX_BYTES_MEMORY).fill("00");
     }
 }
