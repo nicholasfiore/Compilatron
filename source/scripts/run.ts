b6db8cd084344acfbe362d689b4ffbe0f82a7c26
nicholas.fiore1@marist.edu <nicholas.fiore1@marist.edu>
2024-05-06 14:54:13 -0400

    added verbose logging toggle

--- source/scripts/run.ts
+++ source/scripts/run.ts
@@ -3,6 +3,7 @@
 function initializeCompiler() {
     console.log("Initializing Compiler");
     var sourceCode: string = (<HTMLInputElement>document.getElementById("taSourceCode")).value;
-    var _Compiler = new Compiler(sourceCode, true);
+    let debug = document.getElementById("debugSwitch") as HTMLInputElement;
+    var _Compiler = new Compiler(sourceCode, debug.checked)
     _Compiler.compile();
 }
